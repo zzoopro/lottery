9b175680b7abbed2bf97557436c5b81e2f0eb20a
zzoo <zzoolink@gmail.com>
2023-06-23 02:16:50 +0900

    drag 이벤트 구현중중

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,45 @@
+import { motion } from "framer-motion";
 import Footer from "../components/common/Footer";
 import Header from "../components/common/Header";
+import { styled } from "styled-components";
+import { DragEventHandler, MouseEventHandler, useRef } from "react";
+
+const Machine = styled(motion.div)`
+  width: 500px;
+  height: 500px;
+  border: 1px solid red;
+`;
+const Item = styled(motion.div)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 50px;
+  height: 50px;
+  border: 1px solid red;
+  border-radius: 50%;
+  aspect-ratio: 1 / 1;
+`;
 
 const Home = () => {
+  const machineRef = useRef<HTMLDivElement>(null);
+  const items = Array.from({ length: 12 }).map((x, i) => i + 1);
+
+  const onDrag = (event: MouseEvent | TouchEvent, info: any) => {
+    console.log(info);
+  };
+
   return (
     <>
       <Header />
-      <div style={{ height: "80vh" }}></div>
+      <div style={{ height: "80vh" }}>
+        <Machine ref={machineRef}>
+          {items.map((item, i) => (
+            <Item drag dragConstraints={machineRef} dragElastic={0}>
+              {item}
+            </Item>
+          ))}
+        </Machine>
+      </div>
       <Footer />
     </>
   );
