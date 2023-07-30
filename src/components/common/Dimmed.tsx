67ace691f54c60fa699c640dbe8be5dfee0833f7
zzoo <zzoolink@gmail.com>
2023-07-31 02:36:28 +0900

    good

--- src/components/common/Dimmed.tsx
+++ src/components/common/Dimmed.tsx
@@ -12,8 +12,8 @@ const DimmedTag = styled(motion.div)`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
   z-index: 110;
   background-color: rgba(0, 0, 0, 0.6);
 `;
